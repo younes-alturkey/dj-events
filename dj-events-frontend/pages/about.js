e3de8250857bc44b7319b352c5b85cceaf6910e7
يونس التركي <hi@younesalturkey.sa>
2021-12-06 20:38:52 +0300

    Update about.js

--- dj-events-frontend/pages/about.js
+++ dj-events-frontend/pages/about.js
@@ -3,7 +3,7 @@ import Layout from '@/components/Layout'
 export default function AboutPage() {
   return (
     <Layout title="About DJ Events">
-      <h1>About</h1>
+      <h1>About DJ Events</h1>
       <p>This is an app to find the latest DJ and other musical events</p>
     </Layout>
   )
