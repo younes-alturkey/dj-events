1c3e207ed331eec83635026aeeb49429d23f117f
Younes Alturkey <hi@younesalturkey.sa>
2021-12-14 08:11:20 +0300

    add: latest

--- dj-events-frontend/pages/account/dashboard.js
+++ dj-events-frontend/pages/account/dashboard.js
@@ -1,4 +1,41 @@
 import Layout from '@/components/Layout'
-export default function DashboardPage() {
-  return <Layout title="User Dashboard">User Dashboard</Layout>
+import DashboardEvent from '@/components/DashboardEvent'
+import { API_URL } from '@/config/index'
+import { parseCookies } from '@/utilities/index'
+import styles from '@/styles/Dashboard.module.css'
+
+export default function DashboardPage({ events }) {
+  const deleteEvent = (id) => {
+    console.log(id)
+  }
+
+  return (
+    <Layout title="User Dashboard">
+      <div className={styles.dash}>
+        <h1>Dashboard</h1>
+        <h3>My Events</h3>
+
+        {events.map((evt) => (
+          <DashboardEvent key={evt.id} evt={evt} handleDelete={deleteEvent} />
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req)
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const events = await res.json()
+
+  return {
+    props: { events },
+  }
 }
