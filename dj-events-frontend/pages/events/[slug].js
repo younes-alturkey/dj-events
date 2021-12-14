6f3d02eaafcca8e45b8b96462b99656bb7d0b7b7
Younes Alturkey <y.alturkey@ntis.com.sa>
2021-12-14 13:26:43 +0300

    deploy frontend

--- dj-events-frontend/pages/events/[slug].js
+++ dj-events-frontend/pages/events/[slug].js
@@ -38,39 +38,39 @@ export default function EventPage({ evt }) {
   )
 }
 
-export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC`)
-  const events = await res.json()
+// export async function getStaticPaths() {
+//   const res = await fetch(`${API_URL}/events?_sort=date:ASC`)
+//   const events = await res.json()
 
-  const paths = events.map((evt) => ({
-    params: {
-      slug: evt.slug,
-    },
-  }))
+//   const paths = events.map((evt) => ({
+//     params: {
+//       slug: evt.slug,
+//     },
+//   }))
 
-  return {
-    paths,
-    fallback: true,
-  }
-}
+//   return {
+//     paths,
+//     fallback: true,
+//   }
+// }
 
-export async function getStaticProps({ params: { slug } }) {
+// export async function getStaticProps({ params: { slug } }) {
+//   const res = await fetch(`${API_URL}/events?slug=${slug}`)
+//   const events = await res.json()
+//   return {
+//     props: {
+//       evt: events[0],
+//       revalidate: 1,
+//     },
+//   }
+// }
+
+export async function getServerSideProps({ query: { slug } }) {
   const res = await fetch(`${API_URL}/events?slug=${slug}`)
   const events = await res.json()
   return {
     props: {
       evt: events[0],
-      revalidate: 1,
     },
   }
 }
-
-// export async function getServerSideProps({ query: { slug } }) {
-//   const res = await fetch(`${API_URL}/api/events/${slug}`)
-//   const events = await res.json()
-//   return {
-//     props: {
-//       evt: events[0],
-//     },
-//   }
-// }
